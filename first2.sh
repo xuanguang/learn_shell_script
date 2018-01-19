0cbc4e6e94fa40ac7a8bcf9076b4d46f60acf021
LvYiguang[Joshua] <yiguang.lv@hpe.com>
2018-01-19 06:41:51 +0000

    +

---
+++ first2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a comment!
+echo "Hello     World"      # This is a comment, too!
+echo "Hello Wrold"
+echo "Hello * world"
+echo Hello * World
+echo Hello       World
+echo "Hello" World
+echo hello "          " world
+# echo "Hello" "*" World"
+echo "Hello" "*" "World"
+echo `hello` world
+echo 'hello' world
