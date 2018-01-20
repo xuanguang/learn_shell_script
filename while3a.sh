a2d1b9b3c9a701f5cfa59ba2198ddceef28d8f25
LvYiguang[Joshua] <yiguang.lv@hpe.com>
2018-01-20 06:01:28 +0000

    +

---
+++ while3a.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+while read f
+do
+    case $f in
+	hello)           echo English    ;;
+	howdy)           echo American   ;;
+	gday)            echo Austrlian  ;;
+	bonjour)         echo French     ;;
+	"guten tag")     echo German     ;;
+	*)               echo Unknown Language: $f
+			 ;;
+    esac
+done < myfile
+
+    
